10c01d866d3b96c167d87a7483de002e1a8cb919
sidore_m <marc.sidorenko@epita.fr>
2019-05-14 19:06:00 +0200

    Update modern trasher design

--- trasher/trasher.h
+++ trasher/trasher.h
@@ -5,38 +5,70 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-enum mem_type
-{
-  SOFT = 1,
-  HARD = 2,
-  ALL = 3
-};
-
 /* Trasher Pool Manager */
-/*
- * Soft blocks are for temporal memory allocation
- * Hard blocks are for persistant memory (free at exit)
- */
-struct pool_manager
-{
-  struct mem_block *hard;
-  struct mem_block *soft;
+
+struct pool_manager {
+  size_t pools_nb;
+  struct mem_block **pools;
+  char **names;
 };
 
-struct mem_block
-{
+struct mem_block {
   void *data;
   struct mem_block *next;
 };
 
+#if GLOBAL_POOL_MANAGER
+
+/**
+ * Needed before using trash functions
+ */
 void init_pool_manager();
 
-struct pool_manager *get_pool();
+#else
+/**
+ * Get the pool manager structure
+ * @return
+ */
+struct pool_manager *get_pool_manager();
 
-void *mem_s(size_t size);
+#endif
 
-void *mem_h(size_t size);
+/**
+ * Takes the first pool
+ * @param size
+ * @return
+ */
+void *mem(size_t size);
 
-void rm(enum mem_type type);
+/**
+ *
+ * @param size
+ * @param pool_number
+ * @return
+ */
+void *mem_id(size_t size, size_t pool_number);
 
-#endif /* !MEMPOOL_H */
+/**
+ *
+ * @param size
+ * @param pool_name
+ * @return
+ */
+void *mem_name(size_t size, const char *pool_name);
+
+/**
+ * Free the first pool
+ */
+void free_pool();
+
+void free_id(int pool);
+
+void free_name(const char *pool_name);
+
+/**
+ * Free all pools, free the pool_manager
+ */
+void free_pool_all();
+
+#endif /* !TRASHER_H */
