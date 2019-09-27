8682f36e8f23da91c533990b9845ebebea7b1fb7
sidore_m <marc@sidorenko.fr>
2019-09-27 02:44:53 +0200

    Add test: main.c check lib functions

---
+++ test/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "trasher.h"
+
+int main() {
+  printf("Hello World!\n\n");
+
+  size_t buffer_size = 10000;
+  char* buffer = mem(buffer_size);
+
+  int j = 0;
+  for (int i = 0; i < 9999; i++) {
+    buffer[i] = 65 + (j % 24);
+    j++;
+  }
+
+  // printf("%s\n", buffer);
+
+  int* array_IDS = mem_id(33, 5);
+
+  int* array_ids_2 = mem_id(22, 2);
+
+  array_IDS[4] = 25;
+  array_ids_2[3] = 232;
+
+  printf("%d", array_ids_2[3]);
+
+  free_id(2);
+
+  printf("array_IDS access : %d", array_IDS[4]);
+  // printf("array_ids_2 : %d", array_ids_2[3]);
+
+  return 0;
+}
